cbf201fcf9136c54ae969d42752d40ee7a5d2c08
darylfung96 <darylfung96@gmail.com>
2023-11-11 19:12:26 -0600

    Update videoswap.py

--- util/videoswap.py
+++ util/videoswap.py
@@ -85,8 +85,8 @@ def video_swap(video_path, id_vetor, swap_model, detect_model, save_path, temp_r
                 frame_align_crop_tenor_list = []
 
                 # convert swap_model to jit 
-                frame_align_crop_tenor = _totensor(cv2.cvtColor(frame_align_crop_list[0],cv2.COLOR_BGR2RGB))[None,...].to(device).half()
-                swap_model = torch.jit.trace(swap_model, example_inputs=(None, frame_align_crop_tenor, id_vetor, None, True))
+                # frame_align_crop_tenor = _totensor(cv2.cvtColor(frame_align_crop_list[0],cv2.COLOR_BGR2RGB))[None,...].to(device).half()
+                # swap_model = torch.jit.trace(swap_model, example_inputs=(None, frame_align_crop_tenor, id_vetor, None, True))
 
                 for frame_align_crop in frame_align_crop_list:
 
